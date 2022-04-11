eca83c24b3ad81dcbab2ec583a78f1ce89b79619
Roland Faure <rfaure@nosor.irisa.fr>
2022-04-11 11:33:32 +0200

    Rewrote the REAMDE

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # GFA_polish
-Small repository of command lines to polish a GFA assembly. The idea is just to convert to fasta, polish the fasta and reinject in the GFA.
+Very rudimentary script to polish an assembly graph in GFA format. This script does NOT exploit the extra information contained in the GFA relative to the fasta. The idea is just to convert to fasta, polish the fasta and reinject in the GFA.
 
-## Step 1: Convert GFA to fasta
+## All-in-one script
+
+### Dependancies and installation
+You'll need a working python 3 installation. The script runs "awk", which you'll also need in the path (default on Linux distributions). 
+The script also runs a 'rm' at the end: if you do not have the permissions, you can comment out this line or just let it crash, the result won't be affected
+It is a python script thus needs no installation
+
+### Usage
+Give to the script the command you would have used to polish a fasta, replacing the '.fasta' files by '.gfa'. For example, instead of running
+```
+racon reads.fasta alignments.paf assembly.fasta > polished.fasta
+```
+run :
+```
+python gfapolish.py "racon reads.fasta alignments.paf assembly.gfa > polished.gfa "
+
+```
+The script identifies the first GFA of the line as the input file and the last GFA of the line as the output file. 
+If the interface of the polisher is a little complicated (e.g. you do not explicitely give the output file but an output prefix), you'll probably be better off using the manual override below.
+
+## Manual override
+If the above script does not work for one reason or another, here are the two important command lines encapsulated in the script:
+### Step 1: Convert GFA to fasta
 
 ```bash
 awk '/^S/{printf ">"$2; for (i=4; i<=NF;i++) printf "\t"$i;  printf "\n"$3"\n"}' old_gfa.gfa > unpolished.fasta
 ```
 
-## Step 2: Polish the fasta
+### Step 2: Polish the fasta
 
 No special instructions for this one, just choose the polisher.
 
-## Step 3: Inject the polished contigs in the GFA
+### Step 3: Inject the polished contigs in the GFA
 
 ```bash
 awk '{if(FNR==NR){if (substr($1,1,1)==">"){a[0]=NF ; a[1]=substr($1, 2); for (i=2; i<=NF;i++) a[i]="\t"$i}else {printf "S\t"a[1]"\t"$1; for (i=2; i<=a[0];i++) printf a[i]; printf("\n")}}else{if(substr($1,1,1)=="L") {print}}}' polished.fasta old_gfa.gfa > new_gfa.gfa
--- gfapolish.py
+++ gfapolish.py
@@ -13,8 +13,9 @@ def main():
     
     gfaFile = ""
     newargs = []
-    for argument in sys.argv[1:]:
-        
+    args = sys.argv[1].split()
+    #print("argument : " , args)
+    for argument in args:
         if ".gfa" in argument and gfaFile == "" :
             gfaFile = argument.split("=")[-1]
             if len(argument.split("=")) > 1 :
@@ -25,23 +26,53 @@ def main():
         else :
             newargs += [argument]
             
+    polishedFile = ""
+    for arg in range(len(args)-1, -1, -1):
+        if ".gfa" in args[arg] and polishedFile == "" :
+            polishedFile = args[arg].split("=")[-1]
+            if len(args[arg].split("=")) > 1 :
+                newargs[arg] = '='.join(args[arg].split("=")[:-1])+"=polished.fasta"
+            else :
+                newargs[arg] = "polished.fasta"
+            
     if gfaFile == "" :
-        print("ERROR: did not find the .gfa file of your command line")
+        print("ERROR: did not find the input .gfa file in your command line")
+        sys.exit()
+        
+    if polishedFile == "" or polishedFile == gfaFile :
+        print("ERROR: did not find the output .gfa file in your command line")
         sys.exit()
     
     #first convert the GFA to fasta
     commandGFA2fasta = """ awk '/^S/{printf ">"$2; for (i=4; i<=NF;i++) printf "\t"$i;  print ""; print $3;}' """ + gfaFile +""" > unpolished.fasta """
-    os.system(commandGFA2fasta)
+    e = os.system(commandGFA2fasta)
     
     #then polish the fasta
     polishLine = " ".join(newargs)
-    print("command line : ", polishLine)
+    # print("command line : ", polishLine)
+    f = os.system(polishLine)
+    
+    if f != 0 :
+        print("Polishing crashed, here is the command line I used: ", polishLine)
+        sys.exit()
+        
+    out = "polished.fasta"
+    if not os.path.exists("polished.fasta") :
+        if os.path.exists("polished.fasta.fasta") :
+            out = "polished.fasta.fasta"
+        elif os.path.exists("polished.fasta.fa"):
+            out = "polished.fasta.fa"
+        else :
+            print("ERROR: This polisher has an interface too complicated for me. The polished contigs exist somewhere in this directory with a name like 'polished.fasta'. Please switch to manual mode step 3 to finish polishing (see the REAMDE)")
+            sys.exit()
     
     #finally, reinject the polished contigs into the GFA
-    commandfasta2GFA = """ awk '{if(FNR==NR){if (substr($1,1,1)==">"){a[0]=NF ; a[1]=substr($1, 2); for (i=2; i<=NF;i++) a[i]="\t"$i}else {printf "S\t"a[1]"\t"$1; for (i=2; i<=a[0];i++) printf a[i]; print("");}}else{if(substr($1,1,1)=="L") {print}}}' unpolished.fasta """+ gfaFile +""" > new_gfa.gfa """
+    commandfasta2GFA = """ awk '{if(FNR==NR){if (substr($1,1,1)==">"){a[0]=NF ; a[1]=substr($1, 2); for (i=2; i<=NF;i++) a[i]="\t"$i}else {printf "S\t"a[1]"\t"$1; for (i=2; i<=a[0];i++) printf a[i]; print("");}}else{if(substr($1,1,1)=="L") {print}}}' polished.fasta """+ gfaFile +""" > new_gfa.gfa """
     os.system(commandfasta2GFA)
     
     #remove temporary files
+    os.system("rm unpolished.fasta")
+    os.system("rm polished.fasta")
 
 if __name__ == '__main__':
     main()
